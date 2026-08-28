4ed1429224f33f9de84a1757cb511ebbe9dec05a
studentalex228-create <studentalex228@gmail.com>
2026-08-29 00:16:54 +0300

    Refactor settings.gradle.kts for project name change
    Updated project name from 'Media Sanitizer' to 'MediaCleaner' and modified repository management settings.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,29 +1,17 @@
 pluginManagement {
-  repositories {
-    google {
-      content {
-        includeGroupByRegex("com\\.android.*")
-        includeGroupByRegex("com\\.google.*")
-        includeGroupByRegex("androidx.*")
-      }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
-    mavenCentral()
-    gradlePluginPortal()
-  }
 }
-
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
-
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-    maven { url = java.net.URI.create("https://jitpack.io") }
-  }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = java.net.URI("https://jitpack.io") }
+    }
 }
-
-rootProject.name = "Media Sanitizer"
-
-
+rootProject.name = "MediaCleaner"
 include(":app")
